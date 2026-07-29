5fc1c11f6ee807b117686aa5d48965c58b886b57
Prashant Kumar Rajhans <prashantbabu980123@gmail.com>
2026-07-29 16:50:33 +0000

    data typed summary of javascript

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,23 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+console.log("2">1);//true
+console.log("01">1);//false
+
+console.log(null>0);//false
+console.log(null>=0);//true
+console.log(null==0);//false
+
+
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+console.log(undefined==0);//false
+
+//=== strict equality
+console.log("2"==="2");//true
+console.log("2"===2);//false
---
+++ 01_Basics/06_datatypes_summary.js
@@ -0,0 +1,20 @@
+//premetive data types in JavaScript
+let name = "Alice";      // String
+let age = 25;            // Number
+let isStudent = true;    // Boolean
+let score;               // Undefined
+let value = null;        // Null
+let id = Symbol("id");   // Symbol
+let big = 9007199254740991n; // BigInt
+
+//non-premetive data types in JavaScript
+let person = {
+  name: "John",
+  age: 30
+};
+
+let colors = ["Red", "Green", "Blue"];
+
+function greet() {
+  console.log("Hello!");
+}
