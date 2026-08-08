7ae7f2a16de87287d761d4b048508e89bff96659
Prashant Kumar Rajhans <prashantbabu980123@gmail.com>
2026-08-08 13:39:41 +0000

    Add examples of for loops and do-while loops

---
+++ 05_iterators/one.js
@@ -0,0 +1,20 @@
+//for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("\n")
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+
+console.log("\n")
+
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
---
+++ 05_iterators/second.js
@@ -0,0 +1,55 @@
+//while loop
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// console.log("\n");
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// console.log("\n");
+
+// let j = 1;
+// let sum = 0;
+
+// while (j <= 10) {
+//     sum += j;
+//     j++;
+// }
+
+// console.log(sum);
+
+//do while loop
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+// console.log("\n");
+
+let i = 10;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+console.log("\n");
+
+// let i = 2;
+
+// do {
+//     console.log(i);
+//     i += 2;
+// } while (i <= 10);
